ed2ce98477a01d03e19d22f1bc5dd834310fc509
lewiswatson <lewis.watson@bp.com>
2019-02-02 16:59:05 +0000

    Add dice image and link to swift

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
